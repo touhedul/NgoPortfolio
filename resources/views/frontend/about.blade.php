a197351faf8171869215cf48b378ff28aff072d7
sifat <sifatistiak@gmail.com>
2020-07-26 16:30:08 +0600

    about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -8,25 +8,12 @@
         <div class="who-we">
             <div class="row">
                 <div class="col-md-6">
-                    <h3 class="font-normal">who we are</h3>
-                    <h6>Pushing out limits to provide the highest quality services</h6>
-                    <p class="text-justify"> is a development success story – founded in Bangladesh, we are by some
-                        measures the largest non-governmental organisation in the world, an innovator in poverty
-                        eradication and a social enterprise. Demonstrating the power of a holistic, community-based
-                        approach and best known for the scale of our programmes, we have created opportunities for 138
-                        million people.
-                        <br>
-                        <br>
-                        The heroes of our story are people, in particular women struggling in poverty, but the
-                        challenges against them are immense. We are a southern based enterprise committed to working
-                        with communities to design programmes that will effectively and efficiently solve the problems
-                        they have identified.
-                        <br><br>
-
-                        We will concentrate our efforts on three priorities; education and empowerment, equitable
-                        economic development and access to health and water, sanitation and hygiene services.
+                    <h3 class="font-normal">Shataphool Bangladesh: At a glance</h3>
+                    <h6>Where We Started</h6>
+                    <p class="text-justify">Shataphool Bangladesh is a non-government, not-for-profit, non-sectarian, and non-political organization working for social development of vulnerable people who haven’t progressed as they should have.
+                        In 2000, the organization came into being when Mr. M. Shafiqul Islam (UNO of Mohanpur Upazila) founded it with a group of young and energetic people, who were highly motivated and committed to contributing their time, skills and energies for the development and empowerment of socially disadvantaged and vulnerable communities in Mohanpur, addressing poverty issues and eliminating gender based discriminations. By 2001, Shataphool Bangladesh evolved into a formal institution offering micro-credit to poor rural women in Jahanabad Union of Mohanpur Upazila.
+                        Over the next decade the micro credit program diversified, and the organization began providing a variety of micro-credit products for its heterogeneous group of family entrepreneurs, alongside various types of social development programs.
                     </p>
-
 
                 </div>
                 <div class="col-md-6">
@@ -37,15 +24,19 @@
                         <!-- SERVICES -->
                         <li class="col-sm-12"> <i class="fa fa-eye"></i>
                             <h5>our vision</h5>
-                            <p>Phasellus congue lacus eget neque. Phasellus ornare, vitae consectetuer consequat, purus
-                                sapien ultricies dolor,</p>
+                            <p class="text-justify">To ensure the sustainable economic development which provides human rights, mainstreams gender equality, ensures social justice, assures good governance by active participation of multi-tier people of society through poor friendly microfinance services.
+
+                            </p>
+
                         </li>
 
                         <!-- SERVICES -->
                         <li class="col-sm-12"> <i class="fa fa-rocket"></i>
                             <h5>our mission</h5>
-                            <p>Phasellus congue lacus eget neque. Phasellus ornare, vitae consectetuer consequat, purus
-                                sapien ultricies dolor,</p>
+                            <p class="text-justify">To establish an apolitical, cosmopolitan, not-for-profit, socioeconomic and philanthropic private voluntary organization which will work with the mission of making people self-dependent of all the religions and color, especially regenerating those who are impoverished and underprivileged.
+                                
+                            </p>
+
                         </li>
                     </ul>
 
