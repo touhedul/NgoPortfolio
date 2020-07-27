28715acd0ccab5f1cb25f969e53dca915a12a934
sifat <sifatistiak@gmail.com>
2020-07-27 10:59:28 +0600

    slider size

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,7 +6,7 @@
     <ul class="slides">
         @foreach ($sliders as $slider)
         <!-- SLIDER 1 -->
-        <li class="slide-img-1" style="background: url('{{asset('gallery_images/big-'.$slider->image)}}'); background-repeat: no-repeat;  background-size: cover;" data-stellar-background-ratio="0.6">
+        <li class="slide-img-1" style="background: url('{{asset('gallery_images/big-'.$slider->image)}}') 0% 50%; background-repeat: no-repeat;  background-size: cover;" data-stellar-background-ratio="0.6">
             <div class="position-center-center">
                 <!-- <h1>Sotoful</h1>
                 <h5>NGO at your Service</h5>
