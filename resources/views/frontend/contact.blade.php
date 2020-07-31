58c5a7dfa87e68b960241fa025196216bc033f4f
sifat <sifatistiak@gmail.com>
2020-07-31 17:18:04 +0600

    map

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -4,7 +4,7 @@
 @include('includes.banner',['programName'=>'Contact'])
 <section class="padding-top-70">
     <div class="container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58144.53047175591!2d88.57099636236786!3d24.380150026228986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fbefa96a38d031%3A0x10f93a950ed6f410!2sRajshahi!5e0!3m2!1sen!2sbd!4v1595540829142!5m2!1sen!2sbd" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3629.0943437269484!2d88.67794121499675!3d24.551401684200563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fb8ce4ef566df5%3A0x1ccdd5c7addc5c9d!2sJahanabad%20Union%20Council!5e0!3m2!1sen!2sbd!4v1596194213663!5m2!1sen!2sbd" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
 </section>
 <br>
@@ -50,9 +50,11 @@
                     <li class="col-md-12" style="height: inherit;">
                         <article style="height: inherit; padding-top: 90px;">
                             <h5>Head Office</h5>
-                            <p>Jahanabad, Mohanpur, Rajshahi <p>
-                                    <span class="margin-top-30"><i class="fa fa-phone"></i> +8801711062767 </span>
-                                    <span class="primary-color"><i class="fa fa-envelope"></i> shataphool@gmail.com </span>
+                            <p>Jahanabad Union Council
+                            <br> Saipara Bagmara Road
+                             <br> 01719-166634 <p>
+                            <span class="margin-top-30"><i class="fa fa-phone"></i> +8801711062767 </span>
+                            <span class="primary-color"><i class="fa fa-envelope"></i> shataphool@gmail.com </span>
                         </article>
                     </li>
                 </ul>
